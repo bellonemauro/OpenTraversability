58dfda08a40fe33c49a050fe2e4cabc955c622b4
bellonemauro <bellonemauro@gmail.com>
2015-01-21 14:52:52 +0100

    cmake modules

---
+++ CMakeModules/UtilsMacros.cmake
@@ -0,0 +1,153 @@
+# ----------------------------------------------------------------------------
+# Useful macros to define strings and directories 
+# inspired by https://github.com/jlblancoc/mrpt/blob/master/cmakemodules/UtilsMacros.cmake
+# ----------------------------------------------------------------------------
+
+# simple macro to append a _VALUE as suffix to _VAR
+macro(STRING_APPEND  _VAR _VALUE )
+  IF (${_VAR})
+    # not empty, add space and value
+    SET(${_VAR} "${${_VAR}} ${_VALUE}")
+  ELSE(${_VAR})
+    # empty, no space required.
+    SET(${_VAR} "${_VALUE}")
+  ENDIF (${_VAR})
+endmacro(STRING_APPEND)
+
+#-----------------------------------------------------------------------------
+# Only if GNU GCC is used, add one "-isystem" flag for each include directory.
+# Useful to discard -pedantic errors in system libraries not prepared to be so... well, pedantic.
+macro(ADD_DIRECTORIES_AS_ISYSTEM INCLUDE_DIRS)
+	IF($ENV{VERBOSE})
+		MESSAGE(STATUS "isystem: INCLUDE_DIRS= ${${INCLUDE_DIRS}}")
+	ENDIF($ENV{VERBOSE})
+	IF(CMAKE_COMPILER_IS_GNUCXX OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+		FOREACH(DIR ${${INCLUDE_DIRS}})
+			SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -isystem ${DIR}")
+		ENDFOREACH(DIR)
+	ENDIF(CMAKE_COMPILER_IS_GNUCXX OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+endmacro(ADD_DIRECTORIES_AS_ISYSTEM)
+#-----------------------------------------------------------------------------
+
+
+#-----------------------------------------------------------------------------
+# Only if GNU GCC is used, add one "-isystem" flag for each include directory.
+macro(ADD_DIRECTORIES_AS_INCLUDE_AND_ISYSTEM INCLUDE_DIRS)
+	IF($ENV{VERBOSE})
+		MESSAGE(STATUS "isystem: INCLUDE_DIRS= ${${INCLUDE_DIRS}}")
+	ENDIF($ENV{VERBOSE})
+	IF(CMAKE_COMPILER_IS_GNUCXX OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+		FOREACH(DIR ${${INCLUDE_DIRS}})
+			SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -I ${DIR} -isystem ${DIR}")
+		ENDFOREACH(DIR)
+	ENDIF(CMAKE_COMPILER_IS_GNUCXX OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+endmacro(ADD_DIRECTORIES_AS_INCLUDE_AND_ISYSTEM)
+#-----------------------------------------------------------------------------
+
+
+#-----------------------------------------------------------------------------
+# Based on: http://www.cmake.org/pipermail/cmake/2008-February/020114.html
+# Usage: list_subdirectories(the_list_is_returned_here C:/cwd)
+macro(list_subdirectories retval curdir)
+  file(GLOB sub_dir RELATIVE ${curdir} *)
+  set(list_of_dirs "")
+  foreach(dir ${sub_dir})
+    string(SUBSTRING ${dir} 0 1 dir1st)
+    if(IS_DIRECTORY ${curdir}/${dir} AND NOT "${dir1st}" STREQUAL "." AND NOT ${dir} STREQUAL "CMakeFiles")
+        set(list_of_dirs ${list_of_dirs} ${dir})
+    endif(IS_DIRECTORY ${curdir}/${dir} AND NOT "${dir1st}" STREQUAL "." AND NOT ${dir} STREQUAL "CMakeFiles")
+  endforeach(dir)
+  set(${retval} ${list_of_dirs})
+endmacro(list_subdirectories)
+#-----------------------------------------------------------------------------
+
+# Parse a list of "pkg-config"-like flags and split them into INCLUDE_DIRS,etc.
+# Example: pkgconfig_parse("-Ldir1 -llib1 -Idir2" "FOO")
+#  --> FOO_INCLUDE_DIRS = "dir2"
+#  --> FOO_LINK_DIRS = "dir1"
+#  --> FOO_LIBS = "lib1"
+macro(pkgconfig_parse _FLAGS _OUT_PREFIX)
+	string(REPLACE " " ";" _FLAGS_LST ${_FLAGS})
+	SET(${_OUT_PREFIX}_INCLUDE_DIRS "")
+	SET(${_OUT_PREFIX}_LINK_DIRS "")
+	SET(${_OUT_PREFIX}_LIBS "")
+	foreach(str ${_FLAGS_LST})
+		string(LENGTH ${str} _LEN)
+		if (_LEN GREATER 2)
+			string(SUBSTRING ${str} 0 2 _START)
+			MATH( EXPR _LEN2 "${_LEN}-2" )
+			string(SUBSTRING ${str} 2 ${_LEN2} _REST)
+			IF (${_START} STREQUAL "-L")
+				LIST(APPEND ${_OUT_PREFIX}_LINK_DIRS ${_REST})
+			ELSEIF (${_START} STREQUAL "-l")
+				LIST(APPEND ${_OUT_PREFIX}_LIBS ${_REST})
+			ELSEIF (${_START} STREQUAL "-I")
+				LIST(APPEND ${_OUT_PREFIX}_INCLUDE_DIRS ${_REST})
+			ENDIF (${_START} STREQUAL "-L")
+		endif (_LEN GREATER 2)
+	endforeach(str)
+endmacro(pkgconfig_parse )
+
+# Convert a decimal value [0,15] to hexadecimal
+# From: http://stackoverflow.com/questions/26182289/convert-from-decimal-to-hexadecimal-in-cmake
+macro(DECCHAR2HEX VAR VAL)
+        if (${VAL} LESS 10)
+            SET(${VAR} ${VAL})
+        elseif(${VAL} EQUAL 10)
+            SET(${VAR} "A")
+        elseif(${VAL} EQUAL 11)
+            SET(${VAR} "B")
+        elseif(${VAL} EQUAL 12)
+            SET(${VAR} "C")
+        elseif(${VAL} EQUAL 13)
+            SET(${VAR} "D")
+        elseif(${VAL} EQUAL 14)
+            SET(${VAR} "E")
+        elseif(${VAL} EQUAL 15)
+            SET(${VAR} "F")
+        else(${VAL} LESS 10)
+            MESSAGE(FATAL_ERROR "Invalid format for hexidecimal character")
+        endif(${VAL} LESS 10)
+endmacro(DECCHAR2HEX)
+
+# Converts a version like "1.2.3" into a string "0x123"
+# Usage: VERSION_TO_HEXADECIMAL(TARGET_VAR "1.2.3")
+macro(VERSION_TO_HEXADECIMAL  OUT_VAR IN_VERSION)
+	STRING(REGEX MATCHALL "[0-9]+" VERSION_PARTS "${IN_VERSION}")
+	LIST(GET VERSION_PARTS 0 VERSION_NUMBER_MAJOR)
+	LIST(GET VERSION_PARTS 1 VERSION_NUMBER_MINOR)
+	LIST(GET VERSION_PARTS 2 VERSION_NUMBER_PATCH)
+	# Convert each part to hex:
+	DECCHAR2HEX(VERSION_NUMBER_MAJOR_HEX ${VERSION_NUMBER_MAJOR})
+	DECCHAR2HEX(VERSION_NUMBER_MINOR_HEX ${VERSION_NUMBER_MINOR})
+	DECCHAR2HEX(VERSION_NUMBER_PATCH_HEX ${VERSION_NUMBER_PATCH})
+	# Concat version string:
+	SET(${OUT_VAR} "0x${VERSION_NUMBER_MAJOR_HEX}${VERSION_NUMBER_MINOR_HEX}${VERSION_NUMBER_PATCH_HEX}")
+endmacro(VERSION_TO_HEXADECIMAL)
+
+
+# this macros are define to match libraries file name in a specific format
+# Example of usage: 
+#  REMOVE_MATCHING_FILES_FROM_LIST(".*_LIN.cpp" my_srcs)
+
+macro(REMOVE_MATCHING_FILES_FROM_LIST match_expr lst_files)
+	SET(lst_files_aux "")
+	FOREACH(FIL ${${lst_files}})
+		IF(NOT ${FIL} MATCHES "${match_expr}")
+			SET(lst_files_aux "${lst_files_aux}" "${FIL}")
+		ENDIF(NOT ${FIL} MATCHES "${match_expr}")
+	ENDFOREACH(FIL)
+	SET(${lst_files} ${lst_files_aux})
+endmacro(REMOVE_MATCHING_FILES_FROM_LIST)
+
+
+
+macro(KEEP_MATCHING_FILES_FROM_LIST match_expr lst_files)
+	SET(lst_files_aux "")
+	FOREACH(FIL ${${lst_files}})
+		IF(${FIL} MATCHES "${match_expr}")
+			SET(lst_files_aux "${lst_files_aux}" "${FIL}")
+		ENDIF(${FIL} MATCHES "${match_expr}")
+	ENDFOREACH(FIL)
+	SET(${lst_files} ${lst_files_aux})
+endmacro(KEEP_MATCHING_FILES_FROM_LIST)
--- CMakeModules/script_init_cmake.cmake
+++ CMakeModules/script_init_cmake.cmake
@@ -1,1 +1,60 @@
 #cmake init information
+
+#set CMAKEMODULES paths
+SET( CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/CMakeModules;${CMAKE_MODULE_PATH}" )   
+
+#support for shared libraries
+SET_PROPERTY(GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS TRUE)  
+
+# find different libraries for different OSs
+IF(WIN32 OR win64)
+   SET(CMAKE_FIND_LIBRARY_SUFFIXES .lib .dll)
+ELSE()
+   SET(CMAKE_FIND_LIBRARY_SUFFIXES .a .so) #A simple unix configuration for libraries 
+ENDIF()
+
+# Set optimized building (must go BEFORE declaring the targets)
+IF(CMAKE_COMPILER_IS_GNUCXX AND NOT CMAKE_BUILD_TYPE MATCHES "Debug")
+	SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -mtune=native")
+ENDIF(CMAKE_COMPILER_IS_GNUCXX AND NOT CMAKE_BUILD_TYPE MATCHES "Debug")
+
+if(MSVC)
+	ADD_DEFINITIONS(-D_CRT_SECURE_NO_WARNINGS)  # Disable annoying deprecated warnings in MSVC 
+endif(MSVC)
+
+## check for windows 
+if(CMAKE_SYSTEM_NAME MATCHES "Windows")
+ ## Check for Version ##
+    if( ${CMAKE_SYSTEM_VERSION} EQUAL 6.1 ) # Windows 7
+		message ("Windows 7 detected")
+	else( ${CMAKE_SYSTEM_VERSION} EQUAL 6.2 ) # Windows 8
+		message ("Windows 8 detected")
+	endif()
+else() # Some other Windows
+	message ("Windows older than 7 detected")
+endif()
+
+#check for 64 or 32 bit architecture
+if("${CMAKE_SIZEOF_VOID_P}" EQUAL "8")
+	set(BITNESS 64)
+else()
+	set(BITNESS 32)
+endif()
+
+# A function to get all user defined variables with a specified prefix to allow groups in cmake
+function (getListOfVarsStartingWith _prefix _varResult)
+    get_cmake_property(_vars CACHE_VARIABLES)
+    string (REGEX MATCHALL "(^|;)${_prefix}[A-Za-z0-9_]*" _matchedVars "${_vars}")
+    set (${_varResult} ${_matchedVars} PARENT_SCOPE)
+endfunction()
+
+# Setup output Directories 
+set (CMAKE_LIBRARY_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin  CACHE PATH "Single Directory for all Libraries" ) 
+# libraries and executables are going into the same folder to avoid annoying environmental variables in MSVC 
+
+# Setup the Executables output Directory 
+SET (CMAKE_RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin  CACHE PATH "Single Directory for all Executables." )
+
+# Setup the archive output Directory 
+SET (CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin  CACHE PATH "Single Directory for all static libraries.")
+
