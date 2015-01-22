35c55cf5cccdc8bfe9b2dd60dee1b8f3d941ad15
bellonemauro <bellonemauro@gmail.com>
2015-01-22 07:40:45 +0100

    a cmdline app for testing

---
+++ apps/upd_DEMO_cmdline/upd_cmdline_demo.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+#include <ctime>
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/console/parse.h>
+#include <pcl/common/time.h>
+
+#include <ostream>
+
+
+int	main (int argc, char** argv)
+{
+	std::cout<<"\n >>>  UPD cmdline demo 2 experimental acquisition - 2014  <<< \n\n"<<std::endl;
+	
+	if (pcl::console::find_argument (argc, argv, "-v") >= 0)
+   {
+
+   return 0;
+   }
+	if (pcl::console::find_argument (argc, argv, "-b") >= 0)
+   {
+     return 0;
+   }
+	if (pcl::console::find_argument (argc, argv, "") <= 0)
+   {
+     std::cout<<"\nArgument not recognized ... please refer to this help for application usage\n\n"<<std::endl;
+     std::cout<<"-b ---> benchmark \n"<<std::endl;
+     std::cout<<"-v ---> write me \n"<<std::endl;
+    return 0;
+   }
+	return (0);
+}
